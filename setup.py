bf11144e0e51253a19c74a43f9fb1ecfbd48908d
zhaojunhang <zhaojunhang@pointone.tech>
2022-05-07 14:03:33 +0800

    0.1.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "fsbot",
-    version = "0.1.4",
+    version = "0.1.5",
     keywords = ("feishu"),
     description = "fei shu notification",
     long_description = "fei shu notification",
